866691db1eddb7b78bfc7cebb5ba3649098e8c4f
gchevykalov <gcevykalov@gmail.com>
2024-12-09 00:28:39 +0300

    конфиг

--- config.py
+++ config.py
@@ -6,5 +6,3 @@ class Config:
     PORT = os.getenv("TELEGRAM_BOT_PORT", 5000)
     HOST = os.getenv("WEB_HOOK_HOST")
     TELEGRAM_API = "https://api.telegram.org"
-    TELEGRAM_CORE_API_HASH = os.getenv("TELEGRAM_CORE_API_HASH")
-    TELEGRAM_CORE_API_ID = os.getenv("TELEGRAM_CORE_API_ID")
